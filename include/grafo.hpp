8ade4006a55bb35bd7ea7a431e6d07e8d5eba10f
mateusacq <mateus@antinossi.com.br>
2025-09-28 22:16:29 -0300

    feat: construtores do grafo

--- include/grafo.hpp
+++ include/grafo.hpp
@@ -14,44 +14,17 @@
 
 #include <vector> // Para representacao do grafo
 
-typedef struct vertice { // Para representar regioes
-    int dist;
-    int pai;
-    char cor; // b = branco, c = cinza, p = preto
-};
-
 using TipoV = int;
 
 class Grafo {
 public:
-    Grafo(int n);
-    Grafo(const Grafo& outro);
-    Grafo& operator=(const Grafo& outro);
-    ~Grafo();
-
-    void leGrafo(std::ifstream &in);
-
-    void adicionaAresta(TipoV v, TipoV u);
-    void removeAresta(TipoV v, TipoV u);
-    std::vector<TipoV> getAdj(TipoV v);
-    
-    int DistanciaMin(TipoV v, TipoV u);
-
-    bool grafoVazio();
-    void Imprime() const;
+    Grafo();
+    Grafo(int n, int m);
 
 private:
-    int numVertices; // numero de regioes 
-    int numArestas; // numero de ruas 
-
-    // lista de adjacencia (conexoes entre regioes)
-    std::vector<std::vector<TipoV>> arestas; 
-
-    // Metodos internos
-    bool existeAresta(TipoV v, TipoV u) const;
-    bool validaVertice(TipoV v) const;
-
-    std::vector<TipoV> * getRota(TipoV v, TipoV u, vertice * vertices);
+    int numArestas;
+    int numVertices;
+    std::vector<std::vector<TipoV>> arestas;
 };
 
 #endif
--- src/grafo.cpp
+++ src/grafo.cpp
@@ -1,1 +1,7 @@
 #include "grafo.hpp"
+
+Grafo::Grafo() 
+    : numVertices(0), numArestas(0), arestas(0) {}
+
+Grafo::Grafo(int n, int m) 
+    : numVertices(n), numArestas(m), arestas(n) {}
